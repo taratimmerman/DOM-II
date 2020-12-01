e4da39b3a7dc16580e0cc5d957059600cac5d5de
taratimmerman <timmermantara@gmail.com>
2020-12-01 16:40:50 -0700

    Some styiling and functionality complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+
+
+// Middle Sliders
+const contentSection = document.querySelectorAll('.text-content');
+
+function toggleOpen() {
+    this.classList.toggle('open');
+};
+
+function toggleActive(event) {
+    if (event.propertyName.inlcludes('flex')) {
+        this.classList.toggle('open-active');
+    }
+};
+
+contentSection.forEach(slider => slider.addEventListener('click', toggleOpen));
+contentSection.forEach(slider => slider.addEventListener('click', toggleActive));
+
+// Destination fade
+const destination = document.querySelector('.content-destination');
+destination.addEventListener('dblclick', function (event) {
+    this.classList.toggle('active');
+});
